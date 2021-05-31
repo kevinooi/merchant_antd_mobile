64c594dafda5ba4f058438cfff044f92c4b1e5ef
LAPTOP-7PLR680A\kevin <kevinooi1997@gmail.com>
2021-05-31 17:50:42 +0800

    integrate change pw

---
+++ src/components/edit_password_form.tsx
@@ -0,0 +1,302 @@
+import { List, WhiteSpace, Button, Toast, Flex, WingBlank } from 'antd-mobile'
+import { Formik, Form } from 'formik'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import client from '../network/request'
+import { MyInputItem } from './formik_input'
+
+interface FormErrors {
+  password?: string
+  new_password?: string
+  confirm_password?: string
+}
+
+interface FormValues {
+  password?: string
+  new_password: string
+  confirm_password: string
+}
+
+interface FormProps {
+  cancel?: () => void
+}
+
+const EditPasswordForm = ({ cancel }: FormProps) => {
+  const [showPassword, setShowPassword] = useState(false)
+  const [showNewPassword, setShowNewPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+  const history = useHistory()
+
+  return (
+    <Formik
+      initialValues={{ password: '', new_password: '', confirm_password: '' }}
+      validateOnChange={false}
+      validateOnBlur={false}
+      validate={(values: FormValues) => {
+        const errors: FormErrors = {}
+
+        var count = 0
+
+        count += /[a-z]/.test(values.new_password) ? 1 : 0
+        // count += /[A-Z]/.test(values.new_password) ? 1 : 0
+        count += /\d/.test(values.new_password) ? 1 : 0
+        // count += /[@]/.test(values.new_password) ? 1 : 0
+
+        if (!values.password) {
+          errors.password = 'Password is required'
+          Toast.info('Password is required', 1)
+
+          return errors
+        }
+
+        if (!values.new_password) {
+          errors.new_password = 'New password is required'
+          Toast.info('New password is required', 1)
+
+          return errors
+        } else if (count < 2) {
+          errors.new_password =
+            'Password must contain at least one letter, at least one number, and be longer than 8 characters.'
+          Toast.info(
+            'Password must contain at least one letter, at least one number, and be longer than 8 characters.',
+            1
+          )
+
+          return errors
+        }
+
+        if (values.confirm_password != values.new_password) {
+          errors.confirm_password = 'Password does not match'
+          Toast.info('Password does not match', 1)
+        }
+
+        return errors
+      }}
+      onSubmit={async (values, { setSubmitting, setErrors, resetForm }) => {
+        setSubmitting(true)
+
+        const { password, new_password, confirm_password } = values
+
+        // if (code != null) {
+        try {
+          const result = await client.post(`/auth/change-password`, {
+            password: password,
+            new_password: new_password,
+            new_password_confirmation: confirm_password
+          })
+
+          if (result.status != 200) {
+            setErrors({ confirm_password: result.data })
+            Toast.info('Something went wrong, please contact Jomgift', 2)
+          } else {
+            Toast.info('Password updated', 2)
+            resetForm({})
+
+            history.push('/')
+          }
+
+          setSubmitting(false)
+        } catch (error) {
+          let message = 'Password change failed'
+          if (error.response?.data?.message[0]?.messages[0]?.message) {
+            message = error.response?.data?.message[0]?.messages[0]?.message
+          }
+
+          Toast.info(message, 2)
+        }
+        // } else {
+        //   try {
+        // const result = await gqlApi({
+        //   query: `
+        //     mutation UpdateUser($input: updateUserInput) {
+        //         updateUser(input: $input) {
+        //             user {
+        //                 id
+        //                 username
+        //                 email
+        //                 role {
+        //                   id
+        //                   name
+        //                 }
+        //                 profile {
+        //                     id
+        //                     first_name
+        //                     last_name
+        //                     display_name
+        //                     info
+        //                 }
+        //                 places {
+        //                     id
+        //                     name
+        //                 }
+        //                 is_default_password
+        //             }
+        //         }
+        //     }
+        // `,
+        //   variables: {
+        //     input: {
+        //       where: {
+        //         id: user?.id,
+        //       },
+        //       data: {
+        //         password: new_password,
+        //         is_default_password: false,
+        //       },
+        //     },
+        //   },
+        //   jwt: user.jwt,
+        // })
+
+        // let userProfile = result?.data?.updateUser?.user ?? null
+
+        // const newUser = {
+        //   isLoggedIn: true,
+        //   ...userProfile,
+        // }
+
+        // const result2 = await customFetch("/api/login", {
+        //   method: "POST",
+        //   headers: { "Content-Type": "application/json" },
+        //   body: JSON.stringify(newUser),
+        // })
+        // mutate("/api/user", newUser)
+
+        // if (result2.isLoggedIn) {
+        //   Toast.info("Password updated", 2)
+        //   resetForm({})
+        // }
+
+        //     setSubmitting(false)
+        //   } catch (error) {
+        //     let message = 'Password change failed'
+        //     if (error.response?.data?.message[0]?.messages[0]?.message) {
+        //       message = error.response?.data?.message[0]?.messages[0]?.message
+        //     }
+
+        //     Toast.info(message, 2)
+        //   }
+        // }
+
+        // setSubmitting(false)
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleBlur,
+        setFieldValue,
+        setFieldTouched,
+        // handleSubmit,
+        isSubmitting,
+        submitForm
+      }) => (
+        <Form>
+          <List renderHeader={'Current Password'}>
+            <MyInputItem
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              onChange={(value) => setFieldValue('password', value)}
+              onBlur={handleBlur}
+              value={values.password}
+              touched={touched.password}
+              error={errors.password != null}
+              extra={
+                <FontAwesomeIcon
+                  icon={showPassword ? faEye : faEyeSlash}
+                  onClick={() => setShowPassword(!showPassword)}
+                />
+              }
+            />
+          </List>
+
+          <List renderHeader={'New Password'}>
+            <MyInputItem
+              type={showNewPassword ? 'text' : 'password'}
+              name="new_password"
+              onChange={(value) => setFieldValue('new_password', value)}
+              onBlur={handleBlur}
+              value={values.new_password}
+              touched={touched.new_password}
+              error={errors.new_password != null}
+              extra={
+                <FontAwesomeIcon
+                  icon={showNewPassword ? faEye : faEyeSlash}
+                  onClick={() => setShowNewPassword(!showNewPassword)}
+                />
+              }
+            />
+          </List>
+
+          <List
+            renderHeader={'Confirm Password'}
+            renderFooter={
+              'Password must contain at least one letter, at least one number, and be longer than 8 charaters.'
+            }
+          >
+            <MyInputItem
+              type={showConfirmPassword ? 'text' : 'password'}
+              name="confirm_password"
+              onChange={(value) => setFieldValue('confirm_password', value)}
+              onBlur={handleBlur}
+              value={values.confirm_password}
+              touched={touched.confirm_password}
+              error={errors.confirm_password != null}
+              extra={
+                <FontAwesomeIcon
+                  icon={showConfirmPassword ? faEye : faEyeSlash}
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                />
+              }
+            />
+          </List>
+
+          <WhiteSpace />
+
+          <WingBlank>
+            {cancel ? (
+              <Flex>
+                <Flex.Item>
+                  <Button
+                    type="ghost"
+                    style={{ color: '#FD9F13', borderColor: '#FD9F13' }}
+                    onClick={cancel}
+                  >
+                    Cancel
+                  </Button>
+                </Flex.Item>
+                <Flex.Item>
+                  <Button
+                    type="primary"
+                    style={{ background: '#FD9F13', borderColor: '#FD9F13' }}
+                    loading={isSubmitting}
+                    disabled={isSubmitting}
+                    onClick={submitForm}
+                  >
+                    Submit
+                  </Button>
+                </Flex.Item>
+              </Flex>
+            ) : (
+              <Button
+                type="primary"
+                style={{ background: '#FD9F13', borderColor: '#FD9F13' }}
+                loading={isSubmitting}
+                disabled={isSubmitting}
+                onClick={submitForm}
+              >
+                Submit
+              </Button>
+            )}
+          </WingBlank>
+        </Form>
+      )}
+    </Formik>
+  )
+}
+
+export default EditPasswordForm
